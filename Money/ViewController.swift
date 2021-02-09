6d531244a4573bcc8e828c13b3582cf869fcb15e
stavigor <stavigor@mail.ru>
2021-02-10 01:03:03 +0300

    Initial Commit

---
+++ Money/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Money
+//
+//  Created by Даниил Марусенко on 10.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
