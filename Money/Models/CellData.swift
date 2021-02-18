1b1e958eea8e6e557f95911848f45c2f61610d45
stavigor <stavigor@mail.ru>
2021-02-18 14:08:59 +0300

    first commit

---
+++ Money/Models/CellData.swift
@@ -0,0 +1,8 @@
+//
+//  CellData.swift
+//  Money
+//
+//  Created by Даниил Марусенко on 14.02.2021.
+//
+
+import Foundation
---
+++ Money/Models/Transaction.swift
@@ -0,0 +1,8 @@
+//
+//  Transaction.swift
+//  Money
+//
+//  Created by Даниил Марусенко on 10.02.2021.
+//
+
+import Foundation
